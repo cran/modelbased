e329e5cf425fe7c74de4c79c99c00d18a704411c
Dominique Makowski <dom.makowski@gmail.com>
2024-06-11 13:00:02 +0000

    version 0.8.8

--- R/get_marginalmeans.R
+++ R/get_marginalmeans.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 .get_marginalmeans <- function(model,
-                               at = "auto",
+                               by = "auto",
                                ci = 0.95,
                                marginal = FALSE,
                                ...) {
@@ -8,35 +8,36 @@
   insight::check_if_installed("marginaleffects")
 
   # Guess arguments
-  args <- .guess_arguments_means(model, at, ...)
+  my_args <- .guess_arguments_means(model, by, ...)
 
   # Get corresponding datagrid (and deal with particular ats)
-  datagrid <- insight::get_datagrid(model, at = args$at, ...)
+  datagrid <- insight::get_datagrid(model, by = my_args$by, ...)
   # Drop random effects
-  datagrid <- datagrid[insight::find_predictors(model, effects="fixed", flatten = TRUE)]
+  datagrid <- datagrid[insight::find_predictors(model, effects = "fixed", flatten = TRUE)]
   at_specs <- attributes(datagrid)$at_specs
 
-
-  if (marginal == FALSE) {
-    if(insight::is_mixed_model(model)) {
-      means <- marginaleffects::predictions(model,
-                                            newdata=datagrid,
-                                            by=at_specs$varname,
-                                            conf_level = ci,
-                                            re.form=NA)
-    } else {
-      means <- marginaleffects::predictions(model,
-                                            newdata=datagrid,
-                                            by=at_specs$varname,
-                                            conf_level = ci)
-    }
+  if (marginal) {
+    means <- marginaleffects::predictions(model,
+      newdata = insight::get_data(model),
+      by = at_specs$varname,
+      conf_level = ci
+    )
+  } else if (insight::is_mixed_model(model)) {
+    means <- marginaleffects::predictions(model,
+      newdata = datagrid,
+      by = at_specs$varname,
+      conf_level = ci,
+      re.form = NA
+    )
   } else {
     means <- marginaleffects::predictions(model,
-                                          newdata=insight::get_data(model),
-                                          by=at_specs$varname,
-                                          conf_level = ci)
+      newdata = datagrid,
+      by = at_specs$varname,
+      conf_level = ci
+    )
   }
-  attr(means, "at") <- args$at
+  attr(means, "at") <- my_args$by
+  attr(means, "by") <- my_args$by
   means
 }
 
@@ -46,35 +47,39 @@
 
 #' @keywords internal
 .format_marginaleffects_means <- function(means, model, ...) {
+  # check if available
+  insight::check_if_installed("datawizard")
+
   # Format
-  params <- parameters::parameters(means) |>
-    datawizard::data_relocate(c("Predicted", "SE", "CI_low", "CI_high"), after=-1) |>
-    datawizard::data_rename("Predicted", "Mean") |>
-    datawizard::data_remove(c("p", "Statistic", "s.value", "S", "CI")) |>
-    datawizard::data_restoretype(insight::get_data(model))
+  params <- parameters::parameters(means)
+  params <- datawizard::data_relocate(params, c("Predicted", "SE", "CI_low", "CI_high"), after = -1)
+  params <- datawizard::data_rename(params, "Predicted", "Mean")
+  params <- datawizard::data_remove(params, c("p", "Statistic", "s.value", "S", "CI"))
+  params <- datawizard::data_restoretype(params, insight::get_data(model))
 
   # Store info
-  attr(params, "at") <- attr(means, "at")
+  attr(params, "at") <- attr(means, "by")
+  attr(params, "by") <- attr(means, "by")
   params
 }
 
 # Guess -------------------------------------------------------------------
 
 #' @keywords internal
-.guess_arguments_means <- function(model, at = NULL, ...) {
+.guess_arguments_means <- function(model, by = NULL, ...) {
   # Gather info and data from model
   predictors <- insight::find_predictors(model, flatten = TRUE, ...)
-  data <- insight::get_data(model)
+  model_data <- insight::get_data(model)
 
-  # Guess arguments ('at' and 'fixed')
-  if (!is.null(at) && length(at) == 1 && at == "auto") {
+  # Guess arguments ('by' and 'fixed')
+  if (identical(by, "auto")) {
     # Find categorical predictors
-    at <- predictors[!sapply(data[predictors], is.numeric)]
-    if (!length(at) || all(is.na(at))) {
-      stop("Model contains no categorical factor. Please specify 'at'.", call. = FALSE)
+    by <- predictors[!vapply(model_data[predictors], is.numeric, logical(1))]
+    if (!length(by) || all(is.na(by))) {
+      insight::format_error("Model contains no categorical factor. Please specify 'by'.")
     }
-    message("We selected `at = c(", toString(paste0('"', at, '"')), ")`.")
+    insight::format_alert("We selected `by = c(", toString(paste0('"', by, '"')), ")`.")
   }
 
-  list(at=at)
+  list(by = by)
 }
